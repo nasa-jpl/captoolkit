af2808971a05deac70129239afdcc0f71e20e981
fspaolo <fspaolo@gmail.com>
2017-10-11 10:50:51 -0700

    renamed merge2.py -> merge.py

--- captoolbox/merge.py
+++ captoolbox/merge.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 """
-Merges several HDF5 files into a single or multiple larger file(s).
+Merges several HDF5 files into a single file or multiple larger files.
 
 Example
-    python merge.py infiles*.h5 -o outfile.h5
-
-    python merge.py infiles*.h5 -o outfile.h5 -m 3
+    merge.py ifiles_*.h5 -o ofile.h5
+    merge.py ifiles_*.h5 -o ofile.h5 -m 5 -n 5
 
 Notes
+    * The parallel option (-n) only works for multiple outputs (-m)!
     * It merges files in the order they are read.
-    * The parallel option only works with the -m option!
+    # If receive "Argument list too long", pass a string.
     * See complementary program: split.py
 
 """
@@ -21,114 +21,130 @@ import numpy as np
 from glob import glob
 
 
-# Pass command-line arguments
-parser = argparse.ArgumentParser(
-        description='Merges several HDF5 files.')
+def get_args():
+    """ Pass command-line arguments. """
+    parser = argparse.ArgumentParser(
+            description='Merges several HDF5 files.')
+    parser.add_argument(
+            'file', metavar='file', type=str, nargs='+',
+            help='HDF5 files to merge')
+    parser.add_argument(
+            '-o', metavar='ofile', dest='ofile', type=str, nargs=1,
+            help=('output file name'),
+            default=[None], required=True,)
+    parser.add_argument(
+            '-m', metavar='nfiles', dest='nfiles', type=int, nargs=1,
+            help=('number of merged files (blocks)'),
+            default=[1],)
+    parser.add_argument(
+            '-v', metavar='var', dest='vnames', type=str, nargs='+',
+            help=('only merge specific vars if given, otherwise merge all'),
+            default=[],)
+    parser.add_argument(
+            '-z', metavar=None, dest='comp', type=str, nargs=1,
+            help=('compress merged file(s)'),
+            choices=('lzf', 'gzip'), default=[None],)
+    parser.add_argument(
+            '-n', metavar='njobs', dest='njobs', type=int, nargs=1,
+            help=('number of jobs for parallel processing when using -m'),
+            default=[1],)
+    return parser.parse_args()
 
-parser.add_argument(
-        'files', metavar='files', type=str, nargs='+',
-        help='HDF5 files to merge')
 
-parser.add_argument(
-        '-o', metavar='outfile', dest='outfile', type=str, nargs=1,
-        help=('output file name'),
-        default=[None], required=True,)
+def get_total_len(ifiles):
+    """ Get total output length from all input files. """
+    N = 0
+    for fn in ifiles:
+        with h5py.File(fn) as f:
+            N += f.values()[0].shape[0]
+    return N
 
-parser.add_argument(
-        '-m', metavar='nfiles', dest='nfiles', type=int, nargs=1,
-        help=('number of merged files (blocks)'),
-        default=[None], required=False,)
 
-parser.add_argument(
-        '-v', metavar='var', dest='vnames', type=str, nargs='+',
-        help=('only merge specific vars if given, otherwise merge all'),
-        default=[],)
+def get_var_names(ifile):
+    """ Return all '/variable' names in the HDF5. """
+    with h5py.File(ifile, 'r') as f:
+        vnames = f.keys()
+    return vnames
 
-parser.add_argument(
-        '-n', metavar='njobs', dest='njobs', type=int, nargs=1,
-        help=('number of jobs for parallel processing when using -m'),
-        default=[1],)
 
-# Global variables
-args = parser.parse_args()
-infiles = args.files[:]
-outfile = args.outfile[0]
-nfiles = args.nfiles[0]
-vnames = args.vnames[:]
-njobs = args.njobs[0]
-
-# In case a string is passed to avoid "Argument list too long"
-if len(infiles) == 1:
-    infiles = glob(infiles[0])
-
-print 'number of files to merge:', len(infiles)
-
-if nfiles > 1:
-
+def get_multi_io(ifiles, ofile, nfiles):
+    """ Construct multiple input/output file names. """
     # List of groups of input files
-    infiles = np.array_split(infiles, nfiles)
-
+    ifiles = [list(arr) for arr in np.array_split(ifiles, nfiles)]
     # List of output file names
-    fname = os.path.splitext(outfile)[0] + '_%02d.h5'
-    outfiles = [(fname % k) for k in xrange(len(infiles))]
-
-else:
-
-    infiles = [infiles]  # list of lists
-    outfiles = [outfile]  # list of strs
+    fname = os.path.splitext(ofile)[0] + '_%02d.h5'
+    ofiles = [(fname % k) for k in xrange(len(ifiles))]
+    return ifiles, ofiles
 
 
-def main(ifiles, ofile):
+def merge(ifiles, ofile, vnames, comp):
+    """
+    Merge variables from several input files into a single file.
 
-    print 'merging files:', len(ifiles), '...'
-    
-    ifiles = list(ifiles)
-    
+    Args:
+        ifiles (list): input file names.
+        ofile (str): output file name.
+        vnames (list): name of vars to merge.
+    """
+    # Get length of output containers (from all input files)
+    print 'Calculating lenght of output from all input files ...'
+    N = get_total_len(ifiles)
+
     with h5py.File(ofile, 'w') as f:
-    
-        # Create resizable output file using info from first input file
-        firstfile = ifiles.pop(0)
-        print firstfile
-        
-        # Merge specific vars if given, otherwise merge all
-        with h5py.File(firstfile) as f2:
-            maxshape = (None,) + f2.values()[0].shape[1:]
-            variables = vnames if vnames else f2.keys()
-            [f.create_dataset(key, data=f2[key], maxshape=maxshape) \
-                    for key in variables]
-    
-        # Iterate over the remaining input files
-        for infile in ifiles:
-    
-            print infile
-    
-            f2 = h5py.File(infile)
-            length_next = f2.values()[0].shape[0]  # first var
-            length = f.values()[0].shape[0]
-    
-            for key in variables:
-                # Resize the datasets to accommodate next chunk
-                f[key].resize(length + length_next, axis=0)
-    
-                # Write next chunk
-                f[key][length:] = f2[key]
 
-            f.flush()
-            f2.close()
-            length += length_next
+        # Create empty output containers (w/compression optimized for speed)
+        [f.create_dataset(key, (N,), dtype='float64', compression=comp) \
+                for key in vnames]
+
+        # Iterate over the input files
+        k1 = 0
+        for ifile in ifiles:
+            print ifile
     
+            # Write next chunk (the input file)
+            with h5py.File(ifile) as f2:
+                k2 = k1 + f2.values()[0].shape[0]  # first var/first dim
+
+                # Iterate over all variables
+                for key in vnames:
+                    f[key][k1:k2] = f2[key][:]
+
+            k1 = k2
+    
+    print 'merged', len(ifiles), 'files'
     print 'output ->', ofile
 
 
-if njobs == 1:
-    print 'Running sequential code ...'
-    [main(fi, fo) for fi,fo in zip(infiles, outfiles)]
+if __name__ == '__main__':
 
-else:
-    print 'Running parallel code (%d jobs) ...' % njobs
-    from joblib import Parallel, delayed
-    Parallel(n_jobs=njobs, verbose=5)(
-            delayed(main)(fi, fo) for fi,fo in zip(infiles, outfiles))
+    args = get_args() 
+    ifile = args.file[:]  # list
+    ofile = args.ofile[0]  # str
+    nfiles = args.nfiles[0]
+    vnames = args.vnames[:]
+    comp = args.comp[0]
+    njobs = args.njobs[0]
 
+    # In case a string is passed to avoid "Argument list too long"
+    if len(ifile) == 1:
+        ifile = glob(ifile[0])
 
-print 'merged files:', len(infiles)
+    # Get var names from first file, if not provided
+    vnames = get_var_names(ifile[0]) if not vnames else vnames
+
+    # Groups of input files -> multiple output files 
+    if nfiles > 1:
+        ifile, ofile = get_multi_io(ifile, ofile, nfiles)
+    else:
+        ifile, ofile = [ifile], [ofile]
+
+    if njobs > 1 and nfiles > 1:
+        print 'Running parallel code (%d jobs) ...' % njobs
+        from joblib import Parallel, delayed
+        Parallel(n_jobs=njobs, verbose=5)(
+                delayed(merge)(fi, fo, vnames, comp) \
+                        for fi,fo in zip(ifile, ofile))
+    else:
+        print 'Running sequential code ...'
+        [merge(fi, fo, vnames, comp) for fi,fo in zip(ifile, ofile)]
+
