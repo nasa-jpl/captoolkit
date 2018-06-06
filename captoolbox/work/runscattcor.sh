afc45c0f1c1df4200220a0e6081465a010ceb24f
fspaolo <fspaolo@gmail.com>
2018-06-06 10:47:17 -0700

    deleted work

--- captoolbox/work/runscattcor.sh
+++
@@ -1,8 +0,0 @@
-#python scattcor.py -v lon lat h_res t_year -w bs lew tes -d 1 -r 3 -q 3 -p dif -f /mnt/devon-r0/shared_data/ers1/floating_/ANT_ER1_ISHELF_READ_A_RM_TOPO_IBE_TIDE.h5_subset &
-#python scattcor.py -v lon lat h_res t_year -w bs lew tes -d 1 -r 3 -q 3 -p dif -f /mnt/devon-r0/shared_data/ers1/floating_/ANT_ER1_ISHELF_READ_A_RM_TOPO_IBE_TIDE.h5_subset2 &
-#python scattcor.py -v lon lat h_res t_year -w bs lew tes -d 1 -r 3 -q 3 -p dif -f /mnt/devon-r0/shared_data/ers2/floating_/ANT_ER2_ISHELF_READ_A_RM_TOPO_IBE_TIDE.h5_subset &
-#python scattcor.py -v lon lat h_res t_year -w bs lew tes -d 1 -r 3 -q 3 -p dif -f /mnt/devon-r0/shared_data/ers2/floating_/ANT_ER2_ISHELF_READ_A_RM_TOPO_IBE_TIDE.h5_subset2 &
-#python scattcor.py -v lon lat h_res t_year -w bs lew tes -d 1 -r 3 -q 3 -p dif -f /mnt/devon-r0/shared_data/envisat/floating_/ANT_RA2_ISHELF_2002_2010_READ_A_RM_TOPO_IBE_TIDE.h5_subset &
-#python scattcor.py -v lon lat h_res t_year -w bs lew tes -d 1 -r 3 -q 3 -p dif -f /mnt/devon-r0/shared_data/envisat/floating_/ANT_RA2_ISHELF_2002_2010_READ_A_RM_TOPO_IBE_TIDE.h5_subset2 &
-python scattcor.py -v lon lat h_res t_year -w bs lew tes -d 1 -r 5 -q 1 -p dif -f /mnt/devon-r0/shared_data/cryosat2/floating_/ANT_CS2_ISHELF_READ_A_TOPO.h5_subset &
-python scattcor.py -v lon lat h_res t_year -w bs lew tes -d 1 -r 5 -q 1 -p dif -f /mnt/devon-r0/shared_data/cryosat2/floating_/ANT_CS2_ISHELF_READ_A_TOPO.h5_subset2 &
