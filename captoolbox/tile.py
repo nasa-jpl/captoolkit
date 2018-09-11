123c520b2cf1c63b582ab524ec1928f2531b4671
fspaolo <fspaolo@gmail.com>
2018-09-10 21:53:05 -0700

    modified tile.py; now tiles multiple files (in parallel)

--- captoolbox/tile.py
+++ captoolbox/tile.py
@@ -4,6 +4,10 @@ Program for tiling geographical data to reduce data volumes and allow parallizat
 input file which is given the columns for longitude and latitude, overlap (km), block size
 of tiles (km) and the projection used to make the tiles (EPSG-format).
 
+The code can deal with very-large files by reading/searching/saving in chunks.
+
+Large files may slowdown the processing, but it's guarantee to complete the tilling.
+
 Notes:
     bedmap boundaries: -b -3333000 3333000 -3333000 3333000
 
@@ -14,6 +18,7 @@ Change Log:
     - added parallelization
     - added in- and out-of-memory processing
     - added command-line args
+    - added much more...
 
 """
 import os
@@ -24,6 +29,11 @@ import argparse
 import tables as tb
 import pandas as pd
 import numpy as np
+from glob import glob
+
+
+# Optimal chunk size
+chunks = 100000
 
 
 def get_args():
@@ -77,12 +87,8 @@ def print_args(args):
 
 def transform_coord(proj1, proj2, x, y):
     """Transform coordinates from proj1 to proj2 (EPSG num)."""
-    
-    # Set full EPSG projection strings
     proj1 = pyproj.Proj("+init=EPSG:"+proj1)
     proj2 = pyproj.Proj("+init=EPSG:"+proj2)
-
-    # Convert coordinates
     return pyproj.transform(proj1, proj2, x, y)
 
 
@@ -92,38 +98,15 @@ def get_xy(ifile, vnames=['lon', 'lat'], proj='3031'):
     with tb.open_file(ifile) as fi:
         x = fi.get_node('/', xvar)[:]
         y = fi.get_node('/', yvar)[:]
-    x, y = transform_coord('4326', proj, x, y)
-    return x, y
+    return transform_coord('4326', proj, x, y)
 
 
-##DEPRECATED: Always pass a fixed bbox (see below)
-def get_bboxs(x, y, dxy, bboxc=None):
-    """ Define tiles' bbox based on x/y coords tile size. """
-    
-    # If bbox not given, determind from data
-    if bboxc:
-        xmin, xmax, ymin, ymax = bboxc
-    else:
-        xmin, xmax, ymin, ymax = x.min(), x.max(), y.min(), y.max()
-
-    # Number of tile edges on each dimension
-    Nns = int(np.abs(ymax - ymin) / dxy) + 1
-    New = int(np.abs(xmax - xmin) / dxy) + 1
-    
-    # Coord of tile edges for each dimension
-    xg = np.linspace(xmin, xmax, New)
-    yg = np.linspace(ymin, ymax, Nns)
-    
-    # Vector of bbox for each tile   ##NOTE: Nested loop!
-    bboxs = [(w,e,s,n) for w,e in zip(xg[:-1], xg[1:]) 
-                       for s,n in zip(yg[:-1], yg[1:])]
-    del xg, yg
-
-    ###print 'total partitions:', len(bboxs)
-    return bboxs
+def add_suffix(fname, suffix=''):
+    path, ext = os.path.splitext(fname)
+    return path + suffix + ext
 
 
-def get_tile_bbox(grid_bbox, dxy):
+def get_tile_bboxs(grid_bbox, dxy):
     """ Define bbox of tiles given bbox of grid and tile size. """
     
     xmin, xmax, ymin, ymax = grid_bbox
@@ -144,17 +127,16 @@ def get_tile_bbox(grid_bbox, dxy):
     return bboxs
 
 
-def get_tile(ifile, x, y, bbox, buff=1, proj='3031', tile_num=0):
-    """ Get all the data for a specific tile and save to individual file. """
+def get_tile_data(ifile, x, y, bbox, buff=1, proj='3031', tile_num=0):
+    """ Extract data within bbox and save to individual file. """
 
-    # Bounding box
     xmin, xmax, ymin, ymax = bbox
-    
+
     # Open input file (out-of-core)
     fi = tb.open_file(ifile)
 
     # Get all 1d variables into a list (out-of-core)
-    Points = [fi.get_node('/', v.name) for v in fi.list_nodes('/')]
+    points = [fi.get_node('/', v.name) for v in fi.list_nodes('/')]
 
     npts = 0
     nrow = x.shape[0]
@@ -177,34 +159,33 @@ def get_tile(ifile, x, y, bbox, buff=1, proj='3031', tile_num=0):
 
         # Get chunk of data in-memory, and
         # Query chunk in-memory
-        Points_chunk = [d[i:k] for d in Points]  # -> list of 1d chunks
-        Points_chunk = [d[idx] for d in Points_chunk]
+        points_chunk = [d[i:k] for d in points]  # -> list of 1d chunks
+        points_chunk = [d[idx] for d in points_chunk]
 
         # Create output file on first iteration
         if first_iter:
 
             # Define file name
-            sufix = ('_bbox_%d_%d_%d_%d_buff_%g_epsg_%s_tile_%03d' % \
+            suffix = ('_bbox_%d_%d_%d_%d_buff_%g_epsg_%s_tile_%03d' % \
                      (xmin, xmax, ymin, ymax, buff/1e3, proj, tile_num))
 
-            path, ext = os.path.splitext(ifile)
-            ofile = path + sufix + ext
+            ofile = add_suffix(ifile, suffix)
 
             fo = tb.open_file(ofile, 'w')
 
             # Initialize containers (lenght=0)
             out = [fo.create_earray('/', v.name, tb.Float64Atom(), shape=(0,))
-                   for v in Points]
+                   for v in points]
 
             first_iter = False
         
         # Save chunk
-        [v.append(d) for v, d in zip(out, Points_chunk)]
-        npts += Points_chunk[0].shape[0]
+        [v.append(d) for v, d in zip(out, points_chunk)]
+        npts += points_chunk[0].shape[0]
 
         fo.flush()
 
-    print 'tile %03d, #points' % tile_num, npts, '...'
+    if npts != 0: print 'tile %03d: #points' % tile_num, npts, '...'
 
     try:
         fo.close()
@@ -214,8 +195,13 @@ def get_tile(ifile, x, y, bbox, buff=1, proj='3031', tile_num=0):
     fi.close()
 
 
-# Optimal chunk size
-chunks = 10000
+def count_files(ifiles, key='*tile*'):
+    saved = []
+    for f in ifiles: 
+        path, ext = os.path.splitext(f)
+        saved.extend(glob(path + key))
+    return len(saved)
+
 
 # Pass arguments 
 args    = get_args()
@@ -229,27 +215,28 @@ njobs   = args.njobs[0]     # parallel writing
 
 print_args(args)
 
-print 'building bboxes...'
-#bboxs = get_bboxs(x, y, dxy, bbox_)
-bboxs = get_tile_bbox(bbox_, dxy)
+# Generate list of items (only once!) for parallel proc
+print 'generating list of tasks (files x tiles) ...'
 
-##FIXME: This is a workaround for multiple files and parallel proc!!!
-for ifile in ifiles:
+bboxs = get_tile_bboxs(bbox_, dxy)                                       # [b1, b2..]
+xys = [get_xy(f, vnames, proj) for f in ifiles]                          # [(x1,y1), (x2,y2)..]
+fxys = [(f,x,y) for f,(x,y) in zip(ifiles, xys)]                         # [(f1,x1,y1), (f2,x2,y2)..]
+fxybs = [(f,x,y,b,n+1) for (f,x,y) in fxys for n,b in enumerate(bboxs)]  # [(f1,x1,y1,b1,1), (f2,x2,y2,b2,2)..]
 
-    x, y = get_xy(ifile, vnames=vnames, proj=proj)
+print 'number of files:', len(ifiles)
+print 'number of tiles:', len(bboxs)
+print 'number of tasks:', len(fxybs)
 
-    #bboxs = get_bboxs(x, y, dxy, bbox_)
+# For each bbox scan full file
 
-    print 'number of tiles:', len(bboxs)
+if njobs == 1:
+    print 'running sequential code ...'
+    [get_tile_data(f, x, y, b, dr, proj, n) for f,x,y,b,n in fxybs]
 
-    if njobs == 1:
-        print 'running sequential code ...'
-        [get_tile(ifile, x, y, bbox, buff=dr, proj=proj, tile_num=n+1) \
-                for n, bbox in enumerate(bboxs)]
+else:
+    print 'running parallel code (%d jobs) ...' % njobs
+    from joblib import Parallel, delayed
+    Parallel(n_jobs=njobs, verbose=5)(
+            delayed(get_tile_data)(f, x, y, b, dr, proj, n) for f,x,y,b,n in fxybs)  
 
-    else:
-        print 'running parallel code (%d jobs) ...' % njobs
-        from joblib import Parallel, delayed
-        Parallel(n_jobs=njobs, verbose=5)(
-                delayed(get_tile)(ifile, x, y, bbox, buff=dr, proj=proj, tile_num=n+1) \
-                        for n, bbox in enumerate(bboxs))
+print 'number of tiles with data:', count_files(ifiles)
