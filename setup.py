fcc379935013548968425e8e46ccc9b3e2440719
fspaolo <fspaolo@gmail.com>
2019-10-22 14:16:35 -0700

    Remove bin folder

--- setup.py
+++ setup.py
@@ -15,16 +15,17 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/fspaolo/captoolkit',
-    #packages=['captoolkit', 'captoolkit.lib'],  #NOTE: Check this when inclusion of lib
-    packages=setuptools.find_packages(),
+    packages=['captoolkit', 'captoolkit.lib'],  #NOTE: Check this when inclusion of lib
+    #packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=2.7',
     #NOTE 1: Install all python scripts as stand-alone command line utils.
     #NOTE 2: If Anaconda Python, executable scripts will be placed
     # at ~/anaconda3/bin, otherwise the default is /usr/local/bin 
-    scripts=glob('captoolkit/*.py'), 
+    #scripts=glob('captoolkit/*.py'), 
+    scripts=glob('captoolkit/bin/*'), 
 )
