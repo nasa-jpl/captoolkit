061f1db68db97fe29b3727b9fc7292bb92445140
fspaolo <fspaolo@gmail.com>
2020-06-01 12:38:03 -0700

    Remove lib folder from setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import os
 import setuptools
 from glob import glob
 
@@ -6,25 +5,26 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='captoolkit',
+    name="captoolkit",
     version="0.1.0",
-    license='Apache',
-    author='Fernando Paolo and Johan Nilsson',
-    author_email='paolofer@jpl.nasa.gov, johan.nilsson@jpl.nasa.gov',
-    description='JPL Cryosphere Altimetry Processing Toolkit',
+    license="Apache",
+    author="Fernando Paolo and Johan Nilsson",
+    author_email="paolofer@jpl.nasa.gov, johan.nilsson@jpl.nasa.gov",
+    description="JPL Cryosphere Altimetry Processing Toolkit",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/fspaolo/captoolkit',
-    packages=['captoolkit', 'captoolkit.lib'],  #NOTE: Check this when inclusion of lib
-    #packages=setuptools.find_packages(),
+    url="https://github.com/fspaolo/captoolkit",
+    # NOTE: Check `lib` when including in future
+    # packages=['captoolkit', 'captoolkit.lib'],
+    # packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    #NOTE 1: Install all python scripts as stand-alone command line utils.
-    #NOTE 2: If Anaconda Python, executable scripts will be placed
-    # at ~/anaconda3/bin, otherwise the default is /usr/local/bin 
-    scripts=glob('captoolkit/*.py'), 
+    python_requires=">=3.6",
+    # NOTE 1: Install all python scripts as stand-alone command line utils.
+    # NOTE 2: If Anaconda Python, executable scripts will be placed
+    # at ~/anaconda3/bin, otherwise the default is /usr/local/bin
+    scripts=glob("captoolkit/*.py"),
 )
